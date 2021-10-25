113a71bed260f44c45f783033f3f9caa3009d80e
mlovyuan <48782336+mlovyuan@users.noreply.github.com>
2021-10-25 14:10:22 +0800

    implented Set(), Remove() and toString() in LinkedListSample

--- Array/LinkedListSample.cs
+++ Array/LinkedListSample.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Array
 {
@@ -45,7 +42,7 @@ namespace Array
 
         public void Add(int index, T inputElement)
         {
-            if (index < 0 || index > N)
+            if (index < 0 || index >= N)
             {
                 throw new ArgumentException("非法的索引值");
             }
@@ -77,6 +74,132 @@ namespace Array
         public void AddLast(T inputElement)
         {
             Add(N, inputElement);
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= N)
+            {
+                throw new ArgumentException("非法的索引值");
+            }
+
+            Node currentNode = head;
+            for (int i = 0; i < index; i++)
+                currentNode = currentNode.next;
+
+            return currentNode.element;
+        }
+
+        public T GetFirst()
+        {
+            return Get(0);
+        }
+
+        public T GetLast()
+        {
+            return Get(N - 1);
+        }
+
+        public void Set(int index, T newElement)
+        {
+            if (index < 0 || index >= N)
+            {
+                throw new ArgumentException("非法的索引值");
+            }
+
+            Node currentNode = head;
+            for (int i = 0; i < index; i++)
+                currentNode = currentNode.next;
+
+            currentNode.element = newElement;
+        }
+
+        public bool Contains(T element)
+        {
+            Node currentNode = head;
+            while (currentNode != null)
+            {
+                if (currentNode.element.Equals(element))
+                    return true;
+                currentNode = currentNode.next;
+            }
+            return false;
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (index < 0 || index >= N)
+            {
+                throw new ArgumentException("非法的索引值");
+            }
+
+            Node deleteNode;
+            if (index == 0)
+            {
+                deleteNode = head;
+                head = head.next;
+            }
+            else
+            {
+                Node previousNode = head;
+                for (int i = 0; i < index - 1; i++)
+                    previousNode = previousNode.next;
+                deleteNode = previousNode.next;
+                previousNode.next = deleteNode.next;
+            }
+            N--;
+            return deleteNode.element;
+        }
+
+        public T RemoveFirst()
+        {
+            return RemoveAt(0);
+        }
+
+        public T RemoveLast()
+        {
+            return RemoveAt(N - 1);
+        }
+
+        public void Remove(T element)
+        {
+            if (head == null)
+                return;
+            if (head.element.Equals(element))
+            {
+                head = head.next;
+                N--;
+            }
+            else
+            {
+                Node currentNode = head;
+                Node PreviousNode = null;
+                while (currentNode != null)
+                {
+                    if (currentNode.element.Equals(element))
+                        break;
+                    PreviousNode = currentNode;
+                    currentNode = currentNode.next;
+                }
+                if (currentNode != null)
+                {
+                    PreviousNode.next = currentNode.next;
+                    N--;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            Node currentNode = head;
+            while (currentNode != null)
+            {
+                result.Append($"{currentNode} -> ");
+                currentNode = currentNode.next;
+            }
+            result.Append("Null");
+            return result.ToString();
         }
     }
 }
