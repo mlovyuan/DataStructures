83632a26e572dae6abb1b01cc617ab7e5ebbbda7
mlovyuan <mlovyuan@yahoo.com.tw>
2021-11-10 23:26:20 +0800

    Crete StackAndQueue project and IStack Interface

---
+++ StackAndQueue/Stack/ArrayGenericStack.cs
@@ -0,0 +1,22 @@
+﻿namespace StackAndQueue.Stack
+{
+    class ArrayGenericStack<T>:IStack<T>
+    {
+        public int Count { get; }
+        public bool IsEmpty { get; }
+        public void Push()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ StackAndQueue/Stack/IStack.cs
@@ -0,0 +1,11 @@
+﻿namespace StackAndQueue.Stack
+{
+    interface IStack<T>
+    {
+        int Count { get; }
+        bool IsEmpty { get; }
+        void Push();
+        T Pop();
+        T Peek();
+    }
+}
