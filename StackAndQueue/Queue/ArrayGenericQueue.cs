6c1eb1400f9d8ecef5116aeee011d31a80c84109
mlovyuan <mlovyuan@yahoo.com.tw>
2021-11-13 17:15:53 +0800

    Create IQueue Interface and ArrayGenericQueue class

---
+++ StackAndQueue/Queue/ArrayGenericQueue.cs
@@ -0,0 +1,31 @@
+﻿using Array;
+
+namespace StackAndQueue.Queue
+{
+    class ArrayGenericQueue<T>:IQueue<T>
+    {
+        private ArrayGenericSample<T> arrayGeneric;
+
+        public ArrayGenericQueue()
+        {
+            arrayGeneric = new ArrayGenericSample<T>();
+        }
+
+        public int Count { get; }
+        public bool IsEmpty { get; }
+        public void Enqueue(T element)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ StackAndQueue/Queue/IQueue.cs
@@ -0,0 +1,11 @@
+﻿namespace StackAndQueue.Queue
+{
+    interface IQueue<T>
+    {
+        int Count { get; }
+        bool IsEmpty { get; }
+        void Enqueue(T element);
+        T Dequeue();
+        T Peek();
+    }
+}
