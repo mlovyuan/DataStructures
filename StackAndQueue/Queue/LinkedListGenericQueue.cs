cf5cfc68e00edbd602c20be407fd8fb6a9fe0819
mlovyuan <48782336+mlovyuan@users.noreply.github.com>
2021-11-19 14:23:16 +0800

    Add ArrayGenericQueue class

---
+++ StackAndQueue/Queue/LinkedListGenericQueue.cs
@@ -0,0 +1,39 @@
+﻿using Array;
+using System;
+
+namespace StackAndQueue.Queue
+{
+    class LinkedListGenericQueue<T>:IQueue<T>
+    {
+        private LinkedListSample<T> LinkedList;
+
+        public LinkedListGenericQueue()
+        {
+            LinkedList = new LinkedListSample<T>();
+        }
+
+        public int Count => LinkedList.Count;
+
+        public bool IsEmpty => LinkedList.IsEmpty;
+
+        public T Dequeue()
+        {
+            return LinkedList.RemoveFirst();
+        }
+
+        public void Enqueue(T element)
+        {
+            LinkedList.AddLast(element);
+        }
+
+        public T Peek()
+        {
+            return LinkedList.GetFirst();
+        }
+
+        public override string ToString()
+        {
+            return $"Queue front {LinkedList} tail";
+        }
+    }
+}
